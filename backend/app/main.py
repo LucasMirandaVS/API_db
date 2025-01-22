749c46684af750ab81ac0d953ebf21b5f085cb69
Lucas <lucas-mvs@hotmail.com>
2025-01-22 16:44:04 -0300

    backend corrigido: requisições funcionando e banco também

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,21 @@
+import sys
+import os
+
+# Adicionando o diretório `backend` ao caminho de busca de módulos
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../backend')))
+
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import crud, schemas
+from . import crud, schemas  # Corrigido para importar corretamente de 'app'
 from .database import SessionLocal, engine
-import app.models as models # Importando models para criar as tabelas no banco de dados
+from . import models
+
+# O resto do seu código permanece igual
 
 # Criação das tabelas no banco de dados (caso ainda não existam)
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+application = FastAPI()
 
 # Função para obter a sessão do banco de dados
 def get_db():
@@ -18,7 +26,7 @@ def get_db():
         db.close()
 
 # Rota para criar um novo Pokémon
-@app.post("/pokemons/", response_model=schemas.Pokemon)
+@application.post("/pokemons/", response_model=schemas.Pokemon)
 def create_pokemon(pokemon: schemas.PokemonCreate, db: Session = Depends(get_db)):
     try:
         return crud.create_pokemon(db=db, name=pokemon.name)
@@ -26,12 +34,12 @@ def create_pokemon(pokemon: schemas.PokemonCreate, db: Session = Depends(get_db)
         raise HTTPException(status_code=404, detail=str(e))
 
 # Rota para obter todos os Pokémons
-@app.get("/pokemons/", response_model=list[schemas.Pokemon])
+@application .get("/pokemons/", response_model=list[schemas.Pokemon])
 def get_pokemons(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_pokemons(db=db, skip=skip, limit=limit)
 
 # Rota para obter um Pokémon específico pelo ID
-@app.get("/pokemons/{pokemon_id}", response_model=schemas.Pokemon)
+@application.get("/pokemons/{pokemon_id}", response_model=schemas.Pokemon)
 def get_pokemon(pokemon_id: int, db: Session = Depends(get_db)):
     db_pokemon = crud.get_pokemon_by_id(db=db, pokemon_id=pokemon_id)
     if db_pokemon is None:
