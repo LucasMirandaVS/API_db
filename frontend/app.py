9d2569a1801bb8f382ebbdd614056494f39af3f8
Lucas <lucas-mvs@hotmail.com>
2025-01-31 17:03:24 -0300

    Código corrigido

--- frontend/app.py
+++ frontend/app.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, redirect
 import requests
 
 app = Flask(__name__)
 
-# URL do back-end
 API_URL = 'http://localhost:8000'  # Backend Local
 
-# Página inicial com o formulário de busca de Pokémon
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Rota para buscar um Pokémon
+# Busca Pokémon específico
 @app.route('/search', methods=['POST'])
 def search_pokemon():
     pokemon_name = request.form['name']
@@ -21,18 +19,22 @@ def search_pokemon():
             pokemon_data = response.json()
             return render_template('pokedex.html', pokemon=pokemon_data)
         else:
-            return f"Pokemon '{pokemon_name}' não encontrado na API."
+            return render_template('pokedex.html', name=pokemon_name)
     except requests.exceptions.RequestException as e:
         return f"Erro ao se comunicar com o back-end: {str(e)}"
 
-# Rota para exibir a Pokédex com todos os Pokémons no banco
+# Exibe a Pokédex com todos os Pokémons do banco de dados
 @app.route('/pokedex')
 def pokedex():
-    response = requests.get(f'{API_URL}/pokemons/')
-    if response.status_code == 200:
-        pokemons = response.json()
-        return render_template('pokedex.html', pokemons=pokemons)
-    return "Erro ao buscar Pokemons"
+    try:
+        response = requests.get(f'{API_URL}/pokemons/')
+        if response.status_code == 200:
+            pokemons = response.json()
+        else:
+            pokemons = []
+    except requests.exceptions.RequestException:
+        pokemons = []
+    return render_template("pokedex.html", pokemons=pokemons)
 
 if __name__ == '__main__':
     app.run(debug=True)
